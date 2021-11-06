09e7925d4c663dd7faf596803b80d68944ce5db8
Vishal Majumdar <74820625+vishalmajumdar95@users.noreply.github.com>
2021-11-06 17:41:50 +0530

    done

--- controller/carController.js
+++ controller/carController.js
@@ -4,21 +4,29 @@ require('dotenv').config();
 // Create Functions
 exports.carparking = async(req, res) => {
     try {
-        let { carNumber } = req.body
         let carDetails = new CarDB({
-            carNumber
+            carNumber: req.body.carNumber
         })
         await CarDB.find()
             .then((data) => {
-                console.log(data.length);
+                // console.log(data.length);
                 if (data.length === 10) {
                     res.status(400).send({ error: 'Sorry slot is not available', success: false })
                 } else {
-                    carDetails.save()
-                        .then((data) => {
-                            res.status(200).send({ message: `Your car is park at slot Number ${data._id} ` })
-                        }).catch((error) => {
-                            res.status(400).send({ error: error.message, success: false })
+                    CarDB.findOne({ carNumber: req.body.carNumber })
+                        .then((result) => {
+                            if (result === null) {
+                                carDetails.save()
+                                    .then((data) => {
+                                        res.status(200).send({ message: `Your car is park at slot Number ${data._id} ` })
+                                    }).catch((error) => {
+                                        res.status(400).send({ error: error.message, success: false })
+                                    })
+                            } else {
+                                res.status(404).send({ message: "This car is already packed!" })
+                            }
+                        }).catch((err) => {
+                            res.status(400).send({ err: err.message, success: false })
                         })
                 }
             }).catch((err) => {
@@ -44,6 +52,21 @@ exports.unParkedCar = async(req, res) => {
 }
 
 
+exports.getCarById = async(req, res) => {
+    try {
+        let carNumber = req.params.number
+        await CarDB.findById(carNumber)
+            .then((data) => {
+                res.status(200).send(data)
+            })
+            .catch((error) => {
+                res.status(400).send(error)
+            })
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ error: err.message, success: false })
+    }
+}
 exports.getCarById = async(req, res) => {
     try {
         let carNumber = req.params.number
